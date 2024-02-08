648b63a5de7f65c557872d5abef8b69a32ed670b
Dan Desktop <d95tan@gmail.com>
2024-02-08 15:27:21 +0800

    feat: created routes for trip's events

---
+++ controllers/eventsController.js
@@ -0,0 +1,57 @@
+const db = require("../db/index.js");
+
+const validateUser = async (req, res, next) => {
+  const userId = req.user.id;
+  const { tripId } = req.params;
+  try {
+    const trip = await db.query(
+      "SELECT * FROM usertrips WHERE userid=$1 AND tripid=$2",
+      [userId, tripId]
+    );
+    console.log(trip.rows);
+    if (trip.rows.length > 0) {
+      next();
+    } else {
+      res.status(403).json({ error: "Unauthorised access" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Error: error });
+  }
+};
+
+const index = async (req, res) => {
+  const { tripId } = req.params;
+  try {
+    const events = await db.query("SELECT * FROM events WHERE tripid = $1", [
+      tripId,
+    ]);
+    console.log(events.rows);
+    res.json(events.rows);
+  } catch (e) {
+    res.status(500).json({ msg: "Something went wrong", error: e });
+  }
+};
+
+const createEvent = async (req, res) => {
+  const { tripId } = req.params;
+  try {
+    const { name, type, description, start, end } = req.body;
+    console.log(name, type, description, start, end);
+    startD = new Date()
+    endD = new Date()
+    const event = await db.query(
+      'INSERT INTO events (tripid, name, type, description, start, "end") VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      [tripId, name, type, description, start, end]
+    );
+    res.json(event.rows[0]);
+  } catch (e) {
+    res.status(500).json({ msg: "Something went wrong", error: e });
+  }
+};
+
+module.exports = {
+  validateUser,
+  index,
+  createEvent,
+};
--- routes/tripsRouter.js
+++ routes/tripsRouter.js
@@ -2,11 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 const tripsController = require("../controllers/tripsController");
+const eventsController = require("../controllers/eventsController");
 
+//* Trips
 router.get("/", tripsController.index);
-
 router.post("/", tripsController.createTrip);
 
-// router.post("/login", tripsController.login)
+//* Events
+router.get("/:tripId", eventsController.validateUser, eventsController.index);
+router.post(
+  "/:tripId",
+  eventsController.validateUser,
+  eventsController.createEvent
+);
 
 module.exports = router;
